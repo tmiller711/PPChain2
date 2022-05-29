91672740af997a6fc51dd4a070e056046bf2f585
Tristin <tmille2004@gmail.com>
2022-05-29 12:31:04 -0500

    midway through adding bootstrapping

--- src/network.rs
+++ src/network.rs
@@ -5,7 +5,7 @@ use std::fs::File;
 // run name is due to change. Possibly make it main but IDK
 pub fn run()
 {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let listener = TcpListener::bind("192.168.0.21:7878").unwrap();
     
     for stream in listener.incoming() {
         let stream = stream.unwrap();
@@ -16,7 +16,11 @@ pub fn run()
 
 fn findnodes()
 {
-    
+    // send "new node : IP" to a node and have them send back all they nodes they know
+    // have it connect to a seed node (probably my main computer)
+    let mut stream = TcpStream::connect("192.168.0.21:7878").unwrap();
+    stream.write(b"new node : 192.168.0.21:7878").unwrap();
+    stream.flush().unwrap();
 }
 
 fn handle_connection(mut stream: TcpStream) {
@@ -26,17 +30,18 @@ fn handle_connection(mut stream: TcpStream) {
     let msg = String::from_utf8_lossy(&buffer[..]);
     let msg = msg.trim_end_matches("\0");
 
-    // do if statements to see what the msg is and see what the network should do in response
-    // possibly 'if "new node" in msg {}
-    if msg.contains("new node") {
-        newnode(msg.trim_start_matches("new node : "))
-        // add the node to current list and send the node to all nodes that this one already had
-        // send the node to all nodes currently known
-        // if node is not in file then add it
-    }
-}
+    // if it's the first time booting up
+    findnodes();
 
-fn newnode(msg: &str) {
+    // do if statements to see what the msg is and see what the network should do in response
+    if msg.contains("new node") {
+        newnode(msg.trim_start_matches("new node : "), &stream);
+        stream.write(b"accepted").unwrap();
+        stream.flush().unwrap();
+    }
+} 
+
+fn newnode(msg: &str, mut stream: &TcpStream) {
     // check if node is already in file
     let nodesfile = File::open("nodes.txt").unwrap();
     let reader = BufReader::new(nodesfile);
@@ -47,10 +52,14 @@ fn newnode(msg: &str) {
         if line == msg {
             println!("alerady here nigga");
             alreadyknown = true;
-            break;
+        } else {
+            // send all known nodes
+            stream.write(line.as_bytes()).unwrap();
+            stream.flush().unwrap();
         }
     }
 
+    // if its not already in file add it
     if alreadyknown == false {
         let mut nodesfile = File::options().append(true).open("nodes.txt").unwrap();
         let msg = format!("{}{}", msg, "\n");
