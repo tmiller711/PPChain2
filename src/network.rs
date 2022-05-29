dea117a2b453ab0825a3f26b1535d4dc0514de11
Tristin <tmille2004@gmail.com>
2022-05-29 14:21:46 -0500

    fucking retard

--- src/network.rs
+++ src/network.rs
@@ -6,6 +6,9 @@ use std::fs::File;
 pub fn run()
 {
     let listener = TcpListener::bind("192.168.0.21:7878").unwrap();
+
+    // if it's the first time booting up in a while
+    findnodes();
     
     for stream in listener.incoming() {
         let stream = stream.unwrap();
@@ -18,7 +21,7 @@ fn findnodes()
 {
     // send "new node : IP" to a node and have them send back all they nodes they know
     // have it connect to a seed node (probably my main computer)
-    let mut stream = TcpStream::connect("192.168.0.21:7878").unwrap();
+    let mut stream = TcpStream::connect("192.168.0.105:7878").unwrap();
     stream.write(b"new node : 192.168.0.21:7878").unwrap();
     stream.flush().unwrap();
 }
@@ -30,14 +33,17 @@ fn handle_connection(mut stream: TcpStream) {
     let msg = String::from_utf8_lossy(&buffer[..]);
     let msg = msg.trim_end_matches("\0");
 
-    // if it's the first time booting up
-    findnodes();
+    println!("{msg}");
 
     // do if statements to see what the msg is and see what the network should do in response
     if msg.contains("new node") {
         newnode(msg.trim_start_matches("new node : "), &stream);
         stream.write(b"accepted").unwrap();
         stream.flush().unwrap();
+    }
+    if msg.contains("node") {
+        let mut nodesfile = File::options().append(true).open("nodes.txt").unwrap();
+        nodesfile.write_all(msg.trim_start_matches("node : ").as_bytes()).unwrap()
     }
 } 
 
@@ -54,7 +60,8 @@ fn newnode(msg: &str, mut stream: &TcpStream) {
             alreadyknown = true;
         } else {
             // send all known nodes
-            stream.write(line.as_bytes()).unwrap();
+            let node = format!("node : {}", line);
+            stream.write(node.as_bytes()).unwrap();
             stream.flush().unwrap();
         }
     }
